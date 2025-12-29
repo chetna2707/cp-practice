e5fa6e6fdf4e3ca71b2e19bf1c2bd493fe9617ca
chetna <chetnajuneja55@gmail.com>
2025-12-29 18:14:08 +0530

    Add presence check for number k in array
    Implement a program to check if a number k is present in an array for multiple test cases.

--- 800 rated/#8 how muxh does daytonna cost.cpp
+++ 800 rated/#8 how muxh does daytonna cost.cpp
@@ -1,2 +1,42 @@
 // 1878A
-// 
+// for k to be the most occuring element in the subsegment
+// subsegment can be of any size
+// in a subsegment of size 1 conatining k, k is most occuring
+// so if k is present it return yes
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long n, k;
+        cin >> n >> k;
+        long long a[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        
+        int flag = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(a[i] == k)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag)
+        cout << "yes" <<endl;
+        else
+        cout << "no" <<endl;
+    }
+    return 0;
+}
+
+//tc: o[n]
+// sc = o[n]
